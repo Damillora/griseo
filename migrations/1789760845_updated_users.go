6ee9312a1ea4496d2ba057be58ba4ce2f4b137a2
Damillora <damillora@damillora.com>
2026-09-19 02:47:40 +0700

    feat: permissions

---
+++ migrations/1789760845_updated_users.go
@@ -0,0 +1,42 @@
+package migrations
+
+import (
+	"encoding/json/v2"
+
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("_pb_users_auth_")
+		if err != nil {
+			return err
+		}
+
+		// update collection data
+		if err := json.Unmarshal([]byte(`{
+			"createRule": null,
+			"viewRule": ""
+		}`), &collection); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("_pb_users_auth_")
+		if err != nil {
+			return err
+		}
+
+		// update collection data
+		if err := json.Unmarshal([]byte(`{
+			"createRule": "",
+			"viewRule": "id = @request.auth.id"
+		}`), &collection); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	})
+}
