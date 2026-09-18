e1c41d9bfd39ac86d36eb06e9a205aea0f162248
Damillora <damillora@damillora.com>
2026-09-19 02:54:50 +0700

    feat: more adjustment in permissions

---
+++ migrations/1789761198_updated_media.go
@@ -0,0 +1,46 @@
+package migrations
+
+import (
+	"encoding/json/v2"
+
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_2708086759")
+		if err != nil {
+			return err
+		}
+
+		// update collection data
+		if err := json.Unmarshal([]byte(`{
+			"createRule": "@request.auth.id != \"\"",
+			"deleteRule": "@request.auth.id != \"\"",
+			"listRule": "@request.auth.id != \"\"",
+			"updateRule": "@request.auth.id != \"\""
+		}`), &collection); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_2708086759")
+		if err != nil {
+			return err
+		}
+
+		// update collection data
+		if err := json.Unmarshal([]byte(`{
+			"createRule": null,
+			"deleteRule": null,
+			"listRule": null,
+			"updateRule": null
+		}`), &collection); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	})
+}
